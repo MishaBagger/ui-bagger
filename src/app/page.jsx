1ea99fb6dfc37cf30d65056764d418350b6bbe6c
MishaBagger <Mishanya_05@bk.ru>
2026-03-14 23:53:22 +0300

    Шрифт + отступы

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,12 @@
 export default function Home() {
-    return <h1>Hello, Tailwind!</h1>
+    return (
+        <>
+            <h1 className="text-5xl font-bold text-orange-400 text-center mt-32 p-">
+                Hello, Tailwind!
+            </h1>
+            <button className="bg-orange-500 rounded block mt-10 mx-auto border border-white/50 border-solid hover:bg-orange-400">
+                Click me
+            </button>
+        </>
+    )
 }
