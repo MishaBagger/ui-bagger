3e4cac829cf709be7ea52805556bc5ee645f60c4
MishaBagger <Mishanya_05@bk.ru>
2026-03-15 00:00:51 +0300

    Переходы + анимация

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,11 @@ export default function Home() {
             <h1 className="text-5xl font-bold text-orange-400 text-center mt-32 p-">
                 Hello, Tailwind!
             </h1>
-            <button className="bg-orange-500 rounded block mt-10 mx-auto border border-white/50 border-solid hover:bg-orange-400">
+            <button className="bg-orange-500 rounded block mt-10 mx-auto border border-white/50 border-solid hover:bg-orange-400 transition-colors ease-in-out duration-300">
                 Click me
             </button>
 
-            <div className="bg-blue-400 mt-5 w-24 h-32">
+            <div className="bg-blue-400 mt-5 w-24 h-32 animate-pulse">
                 Width & Height
             </div>
         </>
