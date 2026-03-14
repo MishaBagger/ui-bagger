bc8aab859488db899708e9139566c15658b4609b
MishaBagger <Mishanya_05@bk.ru>
2026-03-14 23:56:21 +0300

    Ширина + высота

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,6 +7,10 @@ export default function Home() {
             <button className="bg-orange-500 rounded block mt-10 mx-auto border border-white/50 border-solid hover:bg-orange-400">
                 Click me
             </button>
+
+            <div className="bg-blue-400 mt-5 w-24 h-32">
+                Width & Height
+            </div>
         </>
     )
 }
